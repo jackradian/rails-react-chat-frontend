4f76e4b9bc3d0712016b73f39396b7579b1585aa
Anh Nguyen <anh.nguyen@playnext-lab.co.jp>
2020-03-23 17:48:11 +0900

    Add login page and api

---
+++ src/components/app/app.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./app.css";
+import Login from "../login";
+
+function App() {
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
+}
+
+export default App;
